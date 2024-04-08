b7634a3b1d6ce96c5c515dec7304af3f4416659a
unknown <COREANA@S19D-PEC-0001.gindiana.local>
2024-04-08 17:21:03 -0300

    add base link

--- partials/menu.php
+++ partials/menu.php
@@ -1,16 +1,16 @@
 <aside class="mt-10">
             <nav>
-                <a href="">
+                <a href="<?=$base;?>">
                     <div class="menu-item active">
                         <div class="menu-item-icon">
-                            <img src="assets/images/home-run.png" width="16" height="16" />
+                            <img src="<?=$base;?>/assets/images/home-run.png" width="16" height="16" />
                         </div>
                         <div class="menu-item-text">
                             Home
                         </div>
                     </div>
                 </a>
-                <a href="">
+                <a href="<?=$base;?>/perfil.php">
                     <div class="menu-item">
                         <div class="menu-item-icon">
                             <img src="assets/images/user.png" width="16" height="16" />
@@ -20,10 +20,10 @@
                         </div>
                     </div>
                 </a>
-                <a href="">
+                <a href="<?=$base;?>/amigos.php">
                     <div class="menu-item">
                         <div class="menu-item-icon">
-                            <img src="assets/images/friends.png" width="16" height="16" />
+                            <img src="<?=$base;?>/assets/images/friends.png" width="16" height="16" />
                         </div>
                         <div class="menu-item-text">
                             Amigos
@@ -33,10 +33,10 @@
                         </div>
                     </div>
                 </a>
-                <a href="">
+                <a href="<?=$base;?>/fotos.php">
                     <div class="menu-item">
                         <div class="menu-item-icon">
-                            <img src="assets/images/photo.png" width="16" height="16" />
+                            <img src="<?=$base;?>/assets/images/photo.png" width="16" height="16" />
                         </div>
                         <div class="menu-item-text">
                             Fotos
@@ -44,10 +44,10 @@
                     </div>
                 </a>
                 <div class="menu-splitter"></div>
-                <a href="">
+                <a href="<?=$base;?>/config.php>
                     <div class="menu-item">
                         <div class="menu-item-icon">
-                            <img src="assets/images/settings.png" width="16" height="16" />
+                            <img src="<?=$base;?>/assets/images/settings.png" width="16" height="16" />
                         </div>
                         <div class="menu-item-text">
                             Configurações
