bfd2ab5796907cb0ca6ffbace49c5b385f362f7a
unknown <COREANA@S19D-PEC-0001.gindiana.local>
2024-04-08 17:35:22 -0300

    change the menu select

--- partials/menu.php
+++ partials/menu.php
@@ -1,7 +1,7 @@
 <aside class="mt-10">
             <nav>
                 <a href="<?=$base;?>">
-                    <div class="menu-item active">
+                    <div class="menu-item <?=$activeMenu == 'home' ? 'active' : ''?>">
                         <div class="menu-item-icon">
                             <img src="<?=$base;?>/assets/images/home-run.png" width="16" height="16" />
                         </div>
@@ -11,7 +11,7 @@
                     </div>
                 </a>
                 <a href="<?=$base;?>/perfil.php">
-                    <div class="menu-item">
+                    <div class="menu-item <?=$activeMenu == 'perfil' ? 'active' : ''?>">
                         <div class="menu-item-icon">
                             <img src="assets/images/user.png" width="16" height="16" />
                         </div>
@@ -21,7 +21,7 @@
                     </div>
                 </a>
                 <a href="<?=$base;?>/amigos.php">
-                    <div class="menu-item">
+                    <div class="menu-item <?=$activeMenu == 'amigos' ? 'active' : ''?>">
                         <div class="menu-item-icon">
                             <img src="<?=$base;?>/assets/images/friends.png" width="16" height="16" />
                         </div>
@@ -34,7 +34,7 @@
                     </div>
                 </a>
                 <a href="<?=$base;?>/fotos.php">
-                    <div class="menu-item">
+                    <div class="menu-item <?=$activeMenu == 'fotos' ? 'active' : ''?>">
                         <div class="menu-item-icon">
                             <img src="<?=$base;?>/assets/images/photo.png" width="16" height="16" />
                         </div>
@@ -44,8 +44,8 @@
                     </div>
                 </a>
                 <div class="menu-splitter"></div>
-                <a href="<?=$base;?>/config.php>
-                    <div class="menu-item">
+                <a href="<?=$base;?>/config.php">
+                    <div class="menu-item <?=$activeMenu == 'config' ? 'active' : ''?>">
                         <div class="menu-item-icon">
                             <img src="<?=$base;?>/assets/images/settings.png" width="16" height="16" />
                         </div>
